87b043166da5fb137709fe39af3b8c0e4d5b3ba9
MrTangDev <mrtangdev@gmail.com>
2021-01-19 11:57:10 +0100

    Updates

---
+++ precache-manifest.29fb3abb9adfca3eecdf3fa07e372f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2424403c597b416a5d1e212b8d26b8",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "98f9946d88f01b090802",
+    "url": "/webkurs2020-CICD-React/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f9946d88f01b090802",
+    "url": "/webkurs2020-CICD-React/static/js/main.a310f0db.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
